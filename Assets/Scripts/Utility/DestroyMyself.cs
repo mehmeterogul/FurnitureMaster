cd25fe1c0f976dce7cba448d734bc852cdf26fbc
Berk Bekir Baran <berkbekirbaran@hotmail.com>
2023-07-30 18:30:16 +0300

    Collider Map Polish

---
+++ Assets/Scripts/Utility/DestroyMyself.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMyself : MonoBehaviour
+{
+    public void DestroyMe()
+    {
+        Destroy(gameObject);
+    }
+}
